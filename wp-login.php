76d2d18aa21e876b7366287d9724f0935c88a3eb
ShalHafizh <faishalgalip09@gmail.com>
2023-06-23 11:08:58 +0700

    update web

--- wp-login.php
+++ wp-login.php
@@ -11,13 +11,17 @@
 <link rel="stylesheet" id="login-css" href="/wp-admin/css/login.min.css?ver=6.2.2" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
-		</head>
+	<link rel="icon" href="/wp-content/uploads/2023/06/cropped-LogoMakerCa-16873230673431-32x32.png" sizes="32x32">
+<link rel="icon" href="/wp-content/uploads/2023/06/cropped-LogoMakerCa-16873230673431-192x192.png" sizes="192x192">
+<link rel="apple-touch-icon" href="/wp-content/uploads/2023/06/cropped-LogoMakerCa-16873230673431-180x180.png">
+<meta name="msapplication-TileImage" content="/wp-content/uploads/2023/06/cropped-LogoMakerCa-16873230673431-270x270.png">
+	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-id-id">
 	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Dipersembahkan oleh WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="http://localhost/codingku/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Nama Pengguna atau Alamat Email</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
@@ -35,7 +39,7 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Ingat Saya</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log Masuk">
-									<input type="hidden" name="redirect_to" value="http://localhost/codingku/wp-admin/">
+									<input type="hidden" name="redirect_to" value="/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
